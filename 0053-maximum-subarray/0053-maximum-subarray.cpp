18b57c796ed0f935e8e00f729f17cebef1ff2f84
Piyush Purohit <piyushpurohitoffi10@gmail.com>
2026-08-17 19:09:37 +0530

    Time: 0 ms (100%), Space: 71.8 MB (15.17%) - LeetHub

--- 0053-maximum-subarray/0053-maximum-subarray.cpp
+++ 0053-maximum-subarray/0053-maximum-subarray.cpp
@@ -2,14 +2,19 @@ class Solution {
 public:
     int maxSubArray(vector<int>& nums) {
     int maxSum = nums[0];
-    for (int i = 0; i < nums.size(); i++) {
-        int currentSum = currentSum;
+    int currentSum = nums[0];
+    for (int i = 1; i < nums.size(); i++) {
         if (nums[i] > currentSum + nums[i]) {
             currentSum = nums[i];
         }
         else {
             currentSum = currentSum + nums[i];
         }
+        // Instead of the if-else, we can use:
+        // currentSum = max(nums[i], currentSum + nums[i]);
+        // It chooses between starting a new subarray and continuing the current one.
+
+        // This is also known as Kadane's Algorithm.
         if (currentSum > maxSum) {
             maxSum = currentSum;
             }
