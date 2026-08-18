df103253ecdda707a46c131ae44de934612adde0
Piyush Purohit <piyushpurohitoffi10@gmail.com>
2026-08-18 20:36:46 +0530

    Time: 0 ms (100%), Space: 28.1 MB (95.05%) - LeetHub

--- 0169-majority-element/0169-majority-element.cpp
+++ 0169-majority-element/0169-majority-element.cpp
@@ -1,11 +1,7 @@
 class Solution {
 public:
     int majorityElement(vector<int>& nums) {
-        int z = 0;
         sort (nums.begin(), nums.end());
-        for (int i = 0; i < nums.size(); i++) {
-            z = nums[nums.size()/ 2];
-        }
-        return z;
+        return nums[nums.size() / 2];
     }
 };
