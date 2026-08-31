1393b57c4064ab0d77eb8596ee8f3054731e1d2e
Piyush Purohit <piyushpurohitoffi10@gmail.com>
2026-08-31 19:33:46 +0530

    Time: 78 ms (31.07%), Space: 111.3 MB (62.96%) - LeetHub

--- 0217-contains-duplicate/0217-contains-duplicate.cpp
+++ 0217-contains-duplicate/0217-contains-duplicate.cpp
@@ -1,10 +1,13 @@
 class Solution {
 public:
     bool containsDuplicate(vector<int>& nums) {
-        sort(nums.begin(), nums.end());
-        for (int i = 0; i + 1 < nums.size(); i++) {
-            if (nums[i] == nums[i + 1]) {
+        unordered_set <int> seen;
+        for (int i = 0; i < nums.size(); i++) {
+            if (seen.find(nums[i]) != seen.end()) {
                 return true;
+            }
+            if (seen.find(nums[i]) == seen.end()) {
+                seen.insert(nums[i]);
             }
         }
         return false;
