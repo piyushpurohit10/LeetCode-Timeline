6c4b7fe9834c505636be8bb40813c66553ede04b
Piyush Purohit <piyushpurohitoffi10@gmail.com>
2026-08-31 19:49:47 +0530

    Time: 2 ms (31.7%), Space: 28.1 MB (75.74%) - LeetHub

--- 0169-majority-element/0169-majority-element.cpp
+++ 0169-majority-element/0169-majority-element.cpp
@@ -1,21 +1,7 @@
 class Solution {
 public:
     int majorityElement(vector<int>& nums) {
-        int candiate = nums[0];
-        int count = 1;
-        for (int i = 1; i < nums.size(); i++) {
-            if (count == 0) {
-                candiate = nums[i];
-                count = 1;
-            }
-            else if (nums[i] == candiate) {
-                count++;
-            }
-            else {
-                count--;
-            }
-        }
-        // Boyer - Moore Voting Algorithm
-        return candiate;
+        sort(nums.begin(), nums.end());
+        return nums[nums.size() / 2];
     }
 };
