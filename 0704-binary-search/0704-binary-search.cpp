76e08bccbbc4f98f676a84cf184956a3e77fc251
Piyush Purohit <piyushpurohitoffi10@gmail.com>
2026-09-07 18:41:54 +0530

    Time: 0 ms (100%), Space: 31.2 MB (81.2%) - LeetHub

--- 0704-binary-search/0704-binary-search.cpp
+++ 0704-binary-search/0704-binary-search.cpp
@@ -3,7 +3,7 @@ public:
     int search(vector<int>& nums, int target) {
         int start = 0;
         int end = nums.size() - 1;
-        int mid = start + (end - start / 2);
+        int mid = start + (end - start) / 2;
         while (start <= end) {
             if (nums[mid] == target) {
                 return mid;
@@ -14,7 +14,7 @@ public:
             else {
                 end = mid - 1;
             }
-            mid = start + (end - start / 2);
+            mid = start + (end - start) / 2;
         }
         return -1;
     }
